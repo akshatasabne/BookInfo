f054dba23138703117c5b84c697e8c215e01df91
Akshata <Akshata@LAPTOP-4CVBJOT7>
2023-06-24 15:45:44 +0530

    Add project files.

---
+++ BookInfo/BookDetails.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookInfo
+{
+    internal class BookDetails
+    {
+        int book_id;
+        string Book_name;
+        double price;
+
+        public BookDetails()
+        {
+            book_id = 123 ;
+            Book_name = "Integrity";
+            price = 500;
+        }
+        public BookDetails(int book_id, string book_name, double price)
+        {
+            book_id = book_id;
+            Book_name = book_name;
+            price = price;
+        }
+        public string printdata()
+        {
+            return $" Book_id {book_id} \n book_name {Book_name} \n {price}";
+        }
+    }
+}
---
+++ BookInfo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BookDetails details = new BookDetails();
+            Console.WriteLine(details.printdata());
+            BookDetails details1 = new BookDetails(125,"Risk",450);
+            Console.WriteLine(details1.printdata());
+        }
+    }
+}
